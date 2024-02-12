bbca52cba807d346057844c57f114f0bc9071971
jquezada2022505 <jquezada-2022505@kinal.edu.gt>
2024-02-12 09:13:58 -0600

    Se agrego server

---
+++ models/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.alumnosPath = '/api/alumnos';
+
+        this.middlewares();
+    }
+
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log('Servidor Conectado Exitosamente', this.port);
+        });
+    }
+}
+
+module.exports = Server;
